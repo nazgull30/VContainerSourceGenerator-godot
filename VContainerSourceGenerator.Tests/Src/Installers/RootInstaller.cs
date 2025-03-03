85ec13e311b97fa99e10e9a48de32c347353441b
nazgul <nazgull30@gmail.com>
2025-03-03 11:54:14 +0700

    Version 1.0.2

--- VContainerSourceGenerator.Tests/Src/Installers/RootInstaller.cs
+++ VContainerSourceGenerator.Tests/Src/Installers/RootInstaller.cs
@@ -4,6 +4,7 @@ using VContainer;
 using VContainer.Injectors;
 using VContainer.Unity;
 using VContainerSourceGenerator.Tests.Src.Models;
+using VContainerSourceGenerator.Tests.Src.Models.Test;
 
 public partial class RootInstaller : MonoInstaller
 {
